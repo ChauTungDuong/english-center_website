e3ec79bff8cf51a7865779dd6701488dc49e23ab
Duong <tungduong.forwork@gmail.com>
2025-06-11 00:11:54 +0700

    Fix index.js in Models

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,8 @@
-const User = require("./user");
+const User = require("./User");
 const Student = require("./Student");
 const Parent = require("./Parent");
 const Teacher = require("./Teacher");
-const Class = require("./class");
+const Class = require("./Class");
 const Attendance = require("./Attendance");
 
 module.exports = {
