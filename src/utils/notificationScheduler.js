aeda4ed095b59dbd6a0c97d053ff5bae21e81bfa
Duong <tungduong.forwork@gmail.com>
2025-07-05 13:37:48 +0700

    update

--- src/utils/notificationScheduler.js
+++ src/utils/notificationScheduler.js
@@ -47,7 +47,7 @@ class NotificationScheduler {
 
     // Chạy hàng ngày lúc 8:00 AM để gửi daily notifications
     const dailyJob = cron.schedule(
-      "30 13 * * *",
+      "40 13 * * *",
       async () => {
         console.log("📅 Processing daily auto notifications...");
         try {
